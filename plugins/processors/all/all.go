856089fe6b1a2cfbcbac79e14b979f80cde93ebf
mkwiecienkontaktio <m.kwiecien@kontakt.io>
2018-08-03 09:59:43 +0200

    TrackingIdResolver, all required files, step-by-step documentation

--- plugins/processors/all/all.go
+++ plugins/processors/all/all.go
@@ -8,4 +8,5 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/processors/regex"
 	_ "github.com/influxdata/telegraf/plugins/processors/tagrename"
 	_ "github.com/influxdata/telegraf/plugins/processors/topk"
+	_ "github.com/influxdata/telegraf/plugins/processors/trackingidresolver"
 )
