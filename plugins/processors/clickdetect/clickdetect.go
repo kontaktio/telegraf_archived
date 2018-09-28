21725de323c95fa674b70a2e2754d74510031dea
mkwiecienkontaktio <m.kwiecien@kontakt.io>
2018-09-28 13:58:26 +0200

    kba-157_use_clickid_to_calculate_button_presses_in_telegraf_plugin

---
+++ plugins/processors/clickdetect/clickdetect.go
@@ -0,0 +1,103 @@
+package clickdetect
+
+import (
+	"log"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/metric"
+	"github.com/influxdata/telegraf/plugins/processors"
+)
+
+var sampleConfig = `
+# field_name = "name of field to process"
+# out_field_name = "name of output field"
+# tag_key = "tag to identify object"
+`
+
+type ClickDetect struct {
+	FieldName    string `toml:"field_name"`
+	OutFieldName string `toml:"out_field_name"`
+	TagKey       string `toml:"tag_key"`
+
+	cache map[string]float64
+}
+
+func New() *ClickDetect {
+	lastcalc := ClickDetect{}
+
+	lastcalc.Reset()
+	return &lastcalc
+}
+
+func (p *ClickDetect) Reset() {
+	p.cache = make(map[string]float64)
+}
+
+func (p *ClickDetect) SampleConfig() string {
+	return sampleConfig
+}
+
+func (p *ClickDetect) Description() string {
+	return ""
+}
+
+func (p *ClickDetect) Apply(in ...telegraf.Metric) []telegraf.Metric {
+	result := make([]telegraf.Metric, len(in))
+	for idx, mt := range in {
+		if mt.HasTag(p.TagKey) && mt.HasField(p.FieldName) {
+			tag, _ := mt.GetTag(p.TagKey)
+			fieldVal, _ := mt.GetField(p.FieldName)
+			floatField, typeOk := fieldVal.(float64)
+
+			if !typeOk {
+				log.Printf("E! [processors.clickdetect] Invalid type of field %s", p.FieldName)
+				return in //Wrong type
+			}
+
+			lastValue, exists := p.cache[tag]
+
+			p.cache[tag] = floatField
+			if !exists || (lastValue == floatField) {
+				result[idx] = p.copyAndReplaceField(mt, 0)
+				continue
+			}
+
+			if floatField > lastValue {
+				result[idx] = p.copyAndReplaceField(mt, floatField-lastValue)
+				continue
+			} else if floatField < 10 && lastValue > 250 {
+				diff := 256 - lastValue + floatField
+				result[idx] = p.copyAndReplaceField(mt, diff)
+				continue
+			}
+			result[idx] = p.copyAndReplaceField(mt, 0)
+		} else {
+			result[idx] = mt
+		}
+	}
+	return result
+}
+
+func (p *ClickDetect) copyAndReplaceField(mt telegraf.Metric, newValue float64) telegraf.Metric {
+	newMetric, _ := metric.New(
+		mt.Name(),
+		mt.Tags(),
+		make(map[string]interface{}),
+		mt.Time())
+
+	for k, v := range mt.Fields() {
+		if k != p.FieldName {
+			newMetric.AddField(k, v)
+		} else {
+			newMetric.AddField(p.OutFieldName, newValue)
+		}
+	}
+
+	return newMetric
+}
+
+func init() {
+	processors.Add("clickdetect", func() telegraf.Processor {
+		return New()
+	})
+}
---
+++ plugins/processors/clickdetect/clickdetect_test.go
@@ -0,0 +1,206 @@
+package clickdetect
+
+import (
+	"testing"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/metric"
+	"github.com/stretchr/testify/require"
+)
+
+const FieldToChange = "fieldToChange"
+const ChangedField = "changedField"
+const MetricName = "telemetry"
+
+func TestEmit0WhenClickIdEqual(t *testing.T) {
+	cd := *New()
+	cd.FieldName = FieldToChange
+	cd.OutFieldName = ChangedField
+	cd.TagKey = "topic"
+	cd.Reset()
+
+	var metric2Time, _ = time.ParseDuration("500ms")
+
+	var metric1, _ = metric.New(
+		MetricName,
+		map[string]string{
+			"topic":      "abcd",
+			"anotherTag": "q",
+		},
+		map[string]interface{}{
+			FieldToChange:  float64(45),
+			"anotherField": float64(55),
+		},
+		time.Now(),
+	)
+
+	var metric2, _ = metric.New(
+		MetricName,
+		map[string]string{
+			"topic":      "abcd",
+			"anotherTag": "q",
+		},
+		map[string]interface{}{
+			FieldToChange:  float64(45),
+			"anotherField": float64(55),
+		},
+		time.Now().Add(metric2Time),
+	)
+
+	var result = cd.Apply([]telegraf.Metric{metric1}...)
+	var changedFieldValue, changedFieldExists = result[0].GetField(ChangedField)
+	require.True(t, changedFieldExists)
+	require.Equal(t, float64(0), changedFieldValue)
+	require.False(t, result[0].HasField(FieldToChange))
+
+	result = cd.Apply([]telegraf.Metric{metric2}...)
+	changedFieldValue, changedFieldExists = result[0].GetField(ChangedField)
+	require.True(t, changedFieldExists)
+	require.Equal(t, float64(0), changedFieldValue)
+	require.False(t, result[0].HasField(FieldToChange))
+}
+
+func TestNotEmit0WhenClickIdGreater(t *testing.T) {
+	cd := *New()
+	cd.FieldName = FieldToChange
+	cd.OutFieldName = ChangedField
+	cd.TagKey = "topic"
+	cd.Reset()
+
+	var metric2Time, _ = time.ParseDuration("500ms")
+
+	var metric1, _ = metric.New(
+		MetricName,
+		map[string]string{
+			"topic":      "abcd",
+			"anotherTag": "q",
+		},
+		map[string]interface{}{
+			FieldToChange:  float64(45),
+			"anotherField": float64(55),
+		},
+		time.Now(),
+	)
+
+	var metric2, _ = metric.New(
+		MetricName,
+		map[string]string{
+			"topic":      "abcd",
+			"anotherTag": "q",
+		},
+		map[string]interface{}{
+			FieldToChange:  float64(51),
+			"anotherField": float64(55),
+		},
+		time.Now().Add(metric2Time),
+	)
+
+	var result = cd.Apply([]telegraf.Metric{metric1}...)
+	var changedFieldValue, changedFieldExists = result[0].GetField(ChangedField)
+	require.True(t, changedFieldExists)
+	require.Equal(t, float64(0), changedFieldValue)
+	require.False(t, result[0].HasField(FieldToChange))
+
+	result = cd.Apply([]telegraf.Metric{metric2}...)
+	changedFieldValue, changedFieldExists = result[0].GetField(ChangedField)
+	require.True(t, changedFieldExists)
+	require.Equal(t, float64(6), changedFieldValue)
+	require.False(t, result[0].HasField(FieldToChange))
+}
+
+func TestNotEmit0WhenClickIdRolledBack(t *testing.T) {
+	cd := *New()
+	cd.FieldName = FieldToChange
+	cd.OutFieldName = ChangedField
+	cd.TagKey = "topic"
+	cd.Reset()
+
+	var metric2Time, _ = time.ParseDuration("500ms")
+
+	var metric1, _ = metric.New(
+		MetricName,
+		map[string]string{
+			"topic":      "abcd",
+			"anotherTag": "q",
+		},
+		map[string]interface{}{
+			FieldToChange:  float64(252),
+			"anotherField": float64(55),
+		},
+		time.Now(),
+	)
+
+	var metric2, _ = metric.New(
+		MetricName,
+		map[string]string{
+			"topic":      "abcd",
+			"anotherTag": "q",
+		},
+		map[string]interface{}{
+			FieldToChange:  float64(4),
+			"anotherField": float64(55),
+		},
+		time.Now().Add(metric2Time),
+	)
+
+	var result = cd.Apply([]telegraf.Metric{metric1}...)
+	var changedFieldValue, changedFieldExists = result[0].GetField(ChangedField)
+	require.True(t, changedFieldExists)
+	require.Equal(t, float64(0), changedFieldValue)
+	require.False(t, result[0].HasField(FieldToChange))
+
+	result = cd.Apply([]telegraf.Metric{metric2}...)
+	changedFieldValue, changedFieldExists = result[0].GetField(ChangedField)
+	require.True(t, changedFieldExists)
+	require.Equal(t, float64(8), changedFieldValue)
+	require.False(t, result[0].HasField(FieldToChange))
+}
+
+func TestNotEmit0WhenClickIdLower(t *testing.T) {
+	cd := *New()
+	cd.FieldName = FieldToChange
+	cd.OutFieldName = ChangedField
+	cd.TagKey = "topic"
+	cd.Reset()
+
+	var metric2Time, _ = time.ParseDuration("500ms")
+
+	var metric1, _ = metric.New(
+		MetricName,
+		map[string]string{
+			"topic":      "abcd",
+			"anotherTag": "q",
+		},
+		map[string]interface{}{
+			FieldToChange:  float64(123),
+			"anotherField": float64(55),
+		},
+		time.Now(),
+	)
+
+	var metric2, _ = metric.New(
+		MetricName,
+		map[string]string{
+			"topic":      "abcd",
+			"anotherTag": "q",
+		},
+		map[string]interface{}{
+			FieldToChange:  float64(110),
+			"anotherField": float64(55),
+		},
+		time.Now().Add(metric2Time),
+	)
+
+	var result = cd.Apply([]telegraf.Metric{metric1}...)
+	var changedFieldValue, changedFieldExists = result[0].GetField(ChangedField)
+	require.True(t, changedFieldExists)
+	require.Equal(t, float64(0), changedFieldValue)
+	require.False(t, result[0].HasField(FieldToChange))
+
+	result = cd.Apply([]telegraf.Metric{metric2}...)
+	changedFieldValue, changedFieldExists = result[0].GetField(ChangedField)
+	require.True(t, changedFieldExists)
+	require.Equal(t, float64(0), changedFieldValue)
+	require.False(t, result[0].HasField(FieldToChange))
+}
