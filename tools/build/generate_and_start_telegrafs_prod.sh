e61aeef6133367375ec0dcc0009b923e4bce83dd
mkwiecienkontaktio <m.kwiecien@kontakt.io>
2018-11-09 10:45:19 +0100

    fetching prod scripts from wrong s3 directory  - fix

--- tools/build/generate_and_start_telegrafs_prod.sh
+++ tools/build/generate_and_start_telegrafs_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir /config_generator
-aws s3 cp s3://kontakt-telegraf-config/build-test /config_generator --recursive
+aws s3 cp s3://kontakt-telegraf-config/build-prod /config_generator --recursive
 
 cd /etc/telegraf
 python /config_generator/telegraf_stream_config_generate.py --api-key $API_KEY --influxdb-url ${INFLUXDB_URL:=http://influx.kontakt.io} --influxdb-port ${INFLUXDB_PORT:=8086} --influxdb-username $INFLUXDB_USERNAME --influxdb-password $INFLUXDB_PASSWORD --api-url ${API_URL:=http://api.kontakt.io} --data-collection-interval ${DATA_COLLECTION_INTERVAL:=30s} --flush-interval ${FLUSH_INTERVAL:=10s} --flush-jitter ${FLUSH_JITTER:=0s} --debug ${DEBUG_ENABLED:=true} --log-file ${LOG_FILE_NAME:=/var/log/telegraf-config-gen.log} --mqtt-url ${MQTT_URL:=ssl://ovs.kontakt.io:8083} --telemetry-types ${TELEMETRY_TYPES:=health sensor accelerometer button} --streams-per-telegraf ${STREAMS_PER_TELEGRAF:=100} $(if [ ! -z $VENUE_ID ]; then echo "--api-venue-id $VENUE_ID"; else echo ""; fi;)
