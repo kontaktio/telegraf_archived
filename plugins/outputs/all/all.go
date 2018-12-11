8a1ba437f6c998596e6dd26fc120f4cf2ca2121b
mkwiecienkontaktio <m.kwiecien@kontakt.io>
2018-12-11 09:03:41 +0100

    /stream/xyz/all @ generate_and_start_telegrafs_prod

--- plugins/outputs/all/all.go
+++ plugins/outputs/all/all.go
@@ -1,6 +1,7 @@
 package all
 
 import (
+	_ "github.com/influxdata/telegraf/plugins/outputs/file"
 	_ "github.com/influxdata/telegraf/plugins/outputs/http"
 	_ "github.com/influxdata/telegraf/plugins/outputs/influxdb"
 	_ "github.com/influxdata/telegraf/plugins/outputs/influxdb_v2"
