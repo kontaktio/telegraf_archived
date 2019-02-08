b6014b9126f008b2c9273b1963fee43e8cdca7bd
Paweł Stokowiec <pawel@kontakt.io>
2019-02-08 15:47:58 +0100

    Add 'discard' output.

--- plugins/outputs/all/all.go
+++ plugins/outputs/all/all.go
@@ -6,4 +6,5 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/outputs/influxdb"
 	_ "github.com/influxdata/telegraf/plugins/outputs/influxdb_v2"
 	_ "github.com/influxdata/telegraf/plugins/outputs/mqtt"
+	_ "github.com/influxdata/telegraf/plugins/outputs/discard"
 )
