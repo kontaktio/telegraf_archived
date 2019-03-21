92f68fdb18c67c33c04b9d6f77193a7f183190f9
Paweł Stokowiec <pawel@kontakt.io>
2019-03-21 12:44:21 +0100

    Allow to use variables from point metadata.

--- tools/kapacitor/udf/httpSideload/kapacitor_agent.py
+++ tools/kapacitor/udf/httpSideload/kapacitor_agent.py
@@ -86,8 +86,10 @@ class SideloadHandler(Handler):
                 kwargs[name] = point.fieldsInt[name]
             elif name in point.fieldsDouble:
                 kwargs[name] = point.fieldsDouble[name]
-            elif name in point:
-                kwargs[name] = point[name]
+            elif name == 'database':
+                kwargs[name] = point.database
+            elif name == 'name':
+                kwargs[name] = point.name
 
         sideload = self.store.call_get(self._source.format(**kwargs))
 
