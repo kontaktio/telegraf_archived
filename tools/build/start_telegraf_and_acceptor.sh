84f23d2ca073d6d2fb730d57537b40268ddfc181
Boono <sz33psz@gmail.com>
2019-04-12 19:03:00 +0200

    change dockerfile, change entrypoint

--- tools/build/start_telegraf_and_acceptor.sh
+++ tools/build/start_telegraf_and_acceptor.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
 
 mkdir /config_generator
-aws s3 cp s3://kontakt-telegraf-config/build-$1 /config_generator --recursive
+aws s3 cp s3://kontakt-telegraf-config/telegraf.eventprocessor.$1.conf /
 
-cd /config_generator/event_processor
-npm install
-
-source /config_generator/env
-
-cd /etc/telegraf
-
-echo "API_URL: $API_URL"
-
-python /config_generator/telegraf_event_config_generate.py \
-    --influxdb-url "$INFLUXDB_URL" \
-    --influxdb-port $INFLUXDB_PORT \
-    --influxdb-username "$INFLUXDB_USERNAME" \
-    --influxdb-password "$INFLUXDB_PASSWORD" \
-
-pm2 start -f /usr/bin/telegraf -- --config telegraf.events.conf
-
-cd /config_generator/event_processor
-pm2 start -f node -- index.js
-
-tail -f /var/log/telegraf-config-gen.log
+/usr/bin/telegraf -config /telegraf.eventprocessor.$1.conf
