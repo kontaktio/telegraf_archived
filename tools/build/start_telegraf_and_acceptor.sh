2f8f227ad72899ad2edc518638285897cdfac9c1
Tomasz Słonka <t.slonka@kontakt.io>
2025-05-30 15:28:59 +0200

    Enable profiling

--- tools/build/start_telegraf_and_acceptor.sh
+++ tools/build/start_telegraf_and_acceptor.sh
@@ -7,4 +7,4 @@ if [[ "${CONFIG_FROM_S3}" == "true" ]]; then
     aws s3 cp "${configPath}" "${TELEGRAF_CONFIG_PATH}"
 fi
 
-/usr/bin/telegraf -config "${TELEGRAF_CONFIG_PATH}"
+/usr/bin/telegraf -config "${TELEGRAF_CONFIG_PATH}" --pprof-addr :8088
